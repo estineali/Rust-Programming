9cd946f1b54bfedf7a218d25ce0d0abb88aae824
Muhammad Shahrom Ali <estineali@gmail.com>
2019-09-10 11:31:53 +0500

    playing around with rust | Factorial Program

--- first_program/src/main.rs
+++ first_program/src/main.rs
@@ -1,5 +1,23 @@
 fn main() {
-    println!("Hello, world!");
-    let var :i8 = 30;
-    println!("{}", var);
+    println!("\nHello, world!");
+    let mut var :i8 = 30;
+    println!("This is my variable var with value {}.", var);
+    println!("It is an i8 i.e. Signed 8 bit integer");
+    var = -128;
+    println!("now it's value is {}; which is the lowest value an unsigned 8bit integer can take.\n", var);
+
+    println!("Creating a function for factorial");
+    //Anything higher than 20 gives an overflow error
+    for i in 1..21 {
+    	println!("factorial of {} is {}", i, factorial(i));
+    }
+}
+
+pub fn factorial(n: u64) -> u64 {
+	let mut i = 1;
+
+	for x in 1..n+1 {
+		i = i*x;
+	}
+	i
 }
